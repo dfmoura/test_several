36717c9306ca772dd99826a11d1b4bfba9c0e927
dfmoura <diogomou@gmail.com>
2023-09-19 18:56:25 -0300

    Atualizações

--- c/50/main.c
+++ c/50/main.c
@@ -5,18 +5,20 @@
 #include <locale.h>
 int main() {
     setlocale(LC_ALL, "pt_PT.UTF-8");
-    int num,num2=0,num3,positivos=0,negativos=0,soma;
+    int num,num2=0,num3,positivos=0,negativos=0,soma,multiplo_5=0;
     float perPositivo,perNegativo;
     
     while(1){    
-        printf("Informe quantos números deverão compor sua lista da números inteiros: ");
+
+        printf("Aperte 0 e tecle ENTER para sair do programa ou ...\n");
+        printf("informe o tamanho da nova lista e tecle ENTER para continuar no programa: ");
         scanf("%d",&num);
 
         if(num ==0){break;}
 
         //
         
-        int vector[num];
+        //int vector[num];
         for (int i = 0; i < num; i++) {
             num2++;
             printf("digite o %d° número de %d: ",num2,num);
@@ -24,12 +26,14 @@ int main() {
             if(num3>0) positivos++;
             if(num3<0) negativos++;
             soma+=num3;
+            if(num3 % 5 ==0 && num3>0) multiplo_5++;
         }
         perPositivo=(positivos/(positivos+negativos))*100;
         perNegativo=(negativos/(positivos+negativos))*100;
-        printf("\nTotal de números positivos:%d, que representa %.2f%% do total de números.\n",positivos,perPositivo);
-        printf("Total de números negativos:%d, que representa %.2f%% do total de números.\n",negativos,perNegativo);
-        printf("A média aritmética é %.2f.\n",(float)soma/num);
+        printf("\nTotal de números positivos: %d, que representa(m) %.2f%% do total de nro.(s).\n",positivos,perPositivo);
+        printf("Total de números negativos: %d, que representa(m) %.2f%% do total de nro.(s).\n",negativos,perNegativo);
+        printf("A soma total de %d pela quantidade %d, compreende uma média aritmética de %.2f.\n",soma,num,(float)soma/num);
+        printf("Total de números positivos que são múltiplos de 5 equivale(m) %d nro.(s).\n",multiplo_5);
 
     }
     printf("\n");
