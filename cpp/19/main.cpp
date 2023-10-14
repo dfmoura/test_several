ee581e4d9f95a76aa68dfbea7943e4294558f182
dfmoura <diogomou@gmail.com>
2023-10-14 14:43:43 -0300

    Dados diversos

---
+++ cpp/19/main.cpp
@@ -0,0 +1,18 @@
+// 1047
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+int h1,h2,m1,m2,hora,min;
+cin>>h1>>m1>>h2>>m2;
+hora=h2-h1;
+min=m2-m1;
+
+cout<< "O JOGO DUROU "<<hora<<" HORA(S) E "<<min<<" MINUTO(S)"<<endl;
+
+}
+
