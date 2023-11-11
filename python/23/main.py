f65b600956f85567317dc0a5829156fe3444182f
Diogo Moura <diogomou@gmail.com>
2023-11-10 22:59:24 -0300

    novos dev

---
+++ python/23/main.py
@@ -0,0 +1,28 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+teste = "Aula top   !!!"
+print(len(teste))
+print(teste.capitalize())
+print(teste.count('!')+teste.count('!')+teste.count('!')**2)
+print(teste.startswith("Japao"))
+print(teste.endswith("Rinoceronte"))
+print(teste.isalnum())
+print(teste.isalpha())
+print(teste.islower())
+print(teste.isupper())
+print(teste.lower())
+print(teste.title())
+print(teste.upper())
+
+print(teste.swapcase())
+print(teste.split())
+print(teste.replace("Jogar","Fubetol!"))
+print(teste.find("o"))
+print(teste.ljust(10))
+print(teste.rjust(10))
+print(teste.center(50))
+print(teste.lstrip())
+print(teste.rstrip())
+print('ok')
+print(teste.strip())
